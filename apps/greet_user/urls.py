cfaf58fd7b1f1b0e779288d8a63c262724ccc115
Axeratos <nikitalukianitcares@gmail.com>
2022-10-01 20:15:21 +0300

    Refactoring greetings

--- apps/greet_user/urls.py
+++ apps/greet_user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("greet", views.greet_user),
+    path("", views.greet_user),
+    path("<str:username>", views.greet_user),
 ]
