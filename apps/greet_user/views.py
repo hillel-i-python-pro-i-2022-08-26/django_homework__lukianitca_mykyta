7aa7bd97279c3806ade7b842e92e1751094a8c91
Axeratos <nikitalukianitcares@gmail.com>
2022-10-01 18:53:25 +0300

    Generate name logic added

--- apps/greet_user/views.py
+++ apps/greet_user/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from apps.greet_user.services import generate_fake_name
 
 
-def index():
-    return ""
+def greet_user(request, username: str | None = None):
+    if not username:
+        username = generate_fake_name()
+
+    return render(request, "greet_user/index.html", {"username": username})
