4123ee0a45107ed59c119e2d2dd223e6bfb46ddd
Axeratos <nikitalukianitcares@gmail.com>
2022-10-02 10:25:27 +0300

    View name changed

--- apps/greet_user/views.py
+++ apps/greet_user/views.py
@@ -4,7 +4,7 @@ from webargs import fields
 from apps.greet_user.services import generate_fake_name
 
 
-def greet_user(request, username: str | None = None):
+def greetings(request, username: str | None = None):
     if not username:
         username = generate_fake_name()
     return render(request, "greet_user/index.html", {"username": username})
