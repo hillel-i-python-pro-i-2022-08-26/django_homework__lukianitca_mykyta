14f4bc7c27ff372a2dbca56f98a25e7eea03b23a
Axeratos <nikitalukianitcares@gmail.com>
2022-10-03 21:18:02 +0300

    Urls split by homeworks

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+homework_greetings = [
     path("", views.index, name="index"),
     path("greetings/", views.greetings, name="greetings"),
     path("greetings/<str:username>", views.greetings, name="greetings"),
 ]
+
+
+homework_generate_users = []
+
+
+urlpatterns = homework_greetings + homework_generate_users
