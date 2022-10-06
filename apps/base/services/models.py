89117712479b5d61c305527e54fcceb162701553
Axeratos <nikitalukianitcares@gmail.com>
2022-10-06 16:50:11 +0300

    Typing in user's class added

--- apps/base/services/models.py
+++ apps/base/services/models.py
@@ -1,10 +1,12 @@
 from typing import NamedTuple
 
+from apps.base.services.fake_data_types import T_PASSWORD, T_USERNAME, T_EMAIL
+
 
 class User(NamedTuple):
-    username: str
-    email: str
-    password: str
+    username: T_USERNAME
+    email: T_EMAIL
+    password: T_PASSWORD
 
     def __hash__(self):
         return hash(self.username)
