f75cbfcefbb8fcf420fd6c205752bac84dc2a17e
Axeratos <nikitalukianitcares@gmail.com>
2022-10-06 16:04:28 +0300

    Remake for url patterns

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -3,8 +3,16 @@ from . import views
 
 app_name = "base"
 
-urlpatterns = [
+greet_user_homework = [
     path("", views.index, name="index"),
     path("greetings/", views.greetings, name="greetings"),
     path("greetings/<str:username>", views.greetings),
 ]
+
+
+generate_accounts_homework = [
+    path("unique_users/", views.users_info, name="unique_accounts"),
+    path("unique_users/<int:amount_users>", views.users_info),
+]
+
+urlpatterns = greet_user_homework + generate_accounts_homework
