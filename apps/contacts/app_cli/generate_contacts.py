ca995b19f31ebb037cc4c633a24548c041590ba0
axeratos <nikitalukianitcares@gmail.com>
2022-10-10 19:50:01 +0300

    Optional argument handling in contacts generator

--- apps/contacts/app_cli/generate_contacts.py
+++ apps/contacts/app_cli/generate_contacts.py
@@ -1,12 +1,31 @@
-from django.core.management import BaseCommand
-from apps.contacts import models
+import logging
+
+from django.core.management import BaseCommand, CommandParser
+from apps.contacts.models import Contacts
 from apps.contacts.services import generate_fake_contacts
 
 
 class Command(BaseCommand):
+    help = "Generate and save fake contacts"
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.logger = logging.getLogger("django")
+
+    def add_arguments(self, parser: CommandParser):
+        parser.add_argument(
+            "--amount",
+            type=int,
+            default=20,
+            help="Amount of contacts to generate"
+        )
 
     def handle(self, *args, **options):
         amount_contacts = options["amount_contacts"]
-        contacts = generate_fake_contacts()
+        self.logger.info(f"Contacts to generate: {amount_contacts}")
+        current_amount_contacts = Contacts.objects.all().count()
+        self.logger.info(f"Current amount of contacts: {current_amount_contacts}")
+        for number, contact in enumerate(generate_fake_contacts(amount_contacts), start=1):
+            contact.save()
+            self.logger.info(f"Saved: {number}/{amount_contacts}")
+        self.logger.info(f"{amount_contacts} contacts generated")
