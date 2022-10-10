1fea061c9827f9ab722c7a775e510e17a85df332
axeratos <nikitalukianitcares@gmail.com>
2022-10-10 21:00:25 +0300

    Show contacts view added

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic import ListView
+
+from apps.contacts.models import Contacts
 
 
+class ShowContacts(ListView):
+    model = Contacts
+    template_name = "contacts/show_contacts.html"
+    context_object_name = "contacts"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
