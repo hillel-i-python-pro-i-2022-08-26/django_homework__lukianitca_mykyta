64467c2c760f367aaf6ab63ba6bcd78b2d8117b5
axeratos <nikitalukianitcares@gmail.com>
2022-10-10 14:05:31 +0300

    Model's fields added

--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 
 class Contacts(models.Model):
-    pass
+    contact_name = models.CharField(max_length=120)
+    phone_number = models.CharField(max_length=40)
+    birth_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
