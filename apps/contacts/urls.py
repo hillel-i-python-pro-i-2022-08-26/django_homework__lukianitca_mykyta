bf8de7db25096418f622c8660a625d3e79afa2fd
axeratos <nikitalukianitcares@gmail.com>
2022-10-14 15:39:58 +0300

    Routing for add contact

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from apps.contacts import views
 
 app_name = "contacts"
 
 urlpatterns = [
     path("show-contacts/", views.show_contacts, name="show_contacts"),
+    path("add-contact/", views.add_contact, name='add_contact')
 ]
--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
 
+from apps.contacts.forms import AddContactForm
 from apps.contacts.models import Contacts
 
 
-def show_contacts(request):
+def show_contacts(request: HttpRequest) -> HttpResponse:
     contacts_list = Contacts.objects.all()
     return render(request, "contacts/show_contacts.html", {"title": "Contacts List", "contacts": contacts_list})
+
+
+def add_contact(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = AddContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("contacts:show_contacts")
+        return render(request, "contacts/add_contact.html", {"title": "Add Contact", "form": form})
+    else:
+        form = AddContactForm()
+    return render(request, "contacts/add_contact.html", {"title": "Add Contact", "form": form})
