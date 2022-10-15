0489b939343262e3c8cc6b7651989fbf5a7bb09f
axeratos <nikitalukianitcares@gmail.com>
2022-10-15 17:47:16 +0300

    Routing added

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -6,6 +6,7 @@ app_name = "contacts"
 
 urlpatterns = [
     path("show-contacts/", views.show_contacts, name="show_contacts"),
-    path("detail-contact/<int:user_id>", views.detail_contact, name="detail_contact"),
+    path("detail-contact/<int:contact_id>", views.detail_contact, name="detail_contact"),
     path("add-contact/", views.add_contact, name='add_contact'),
+    path("update-contact/<int:contact_id>", views.update_contact, name="update_contact"),
 ]
