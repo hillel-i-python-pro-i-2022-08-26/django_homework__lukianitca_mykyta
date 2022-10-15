f18ae50a614e450e39d2a9743a6bafdd6e583136
axeratos <nikitalukianitcares@gmail.com>
2022-10-15 11:24:55 +0300

    Form validation added

--- apps/contacts/forms.py
+++ apps/contacts/forms.py
@@ -8,17 +8,23 @@ from apps.contacts import models
 
 
 class AddContactForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.minimal_date = datetime.date(day=1, month=1, year=1910)
+        self.maximal_date = datetime.date(day=1, month=1, year=timezone.now().year - 11)
+
     def clean_birth_date(self):
         date = self.cleaned_data.get("birth_date")
-        minimal_date = {"day": 1, "month": 1, "year": 1910}
-        maximal_date = {"day": 1, "month": 1, "year": timezone.now().year - 11}
-        if not datetime.date(**maximal_date) > date > datetime.date(**minimal_date):
+        if not self.maximal_date > date > self.minimal_date:
             raise ValidationError("This date is invalid")
         return date
 
     class Meta:
-        model = models.Contacts
+        model = models.Contact
         fields = ("contact_name", "phone_number", "birth_date")
         widgets = {
-            'birth_date': forms.DateInput(attrs={"type": "date"})
+            'birth_date': forms.DateInput(attrs={"type": "date",
+                                                 "min": datetime.date(day=1, month=1, year=1910),
+                                                 "max": datetime.date(day=1, month=1, year=timezone.now().year - 11),
+                                                 }),
         }
