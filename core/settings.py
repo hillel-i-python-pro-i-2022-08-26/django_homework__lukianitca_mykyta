2780d963b66fd862f9026308932b044d58c6c598
axeratos <nikitalukianitcares@gmail.com>
2022-11-06 22:19:38 +0200

    Crispy forms and dealing with conflicts

--- core/settings.py
+++ core/settings.py
@@ -56,9 +56,16 @@ LOCAL_APPS = [
     "apps.superuser_hw.apps.SuperuserHwConfig",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = [
+    "crispy_forms",
+    "crispy_bootstrap5",
+]
 
 INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -143,9 +150,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR.joinpath("/")
+STATIC_ROOT = BASE_DIR.joinpath("staticfiles")
 
-STATICFILES_DIRS = [BASE_DIR.joinpath("static")]
+STATICFILES_DIRS = [
+    BASE_DIR.joinpath("apps/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
