33466c542af5865d3ff391d7230f6e9e4730d571
axeratos <nikitalukianitcares@gmail.com>
2022-11-12 09:46:43 +0200

    Field avatar added to user model

--- apps/superuser_hw/models.py
+++ apps/superuser_hw/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from apps.services.image_paths import get_avatar_path_user
 
 
 class User(AbstractUser):
-    pass
+    avatar = models.ImageField(max_length=255, null=True, blank=True, upload_to=get_avatar_path_user)
