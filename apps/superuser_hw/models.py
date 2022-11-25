b6f860e158f86d232a112c23c41993fbb6a10c16
axeratos <nikitalukianitcares@gmail.com>
2022-11-25 12:57:56 +0200

    Model for requests and migration

--- apps/superuser_hw/models.py
+++ apps/superuser_hw/models.py
@@ -11,3 +11,20 @@ class User(AbstractUser):
         blank=True,
         upload_to=get_avatar_path_user,
     )
+
+
+class Request(models.Model):
+    session_key = models.CharField(max_length=100)
+    path = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="requests_info")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    visits_count = models.IntegerField()
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=("session_key", "path", "user"),
+                name="Path with these parameters already exists",
+            ),
+        )
