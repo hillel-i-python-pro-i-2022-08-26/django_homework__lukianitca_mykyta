72628ad691b310e31d90a6d8dffa6c9d931a6224
axeratos <nikitalukianitcares@gmail.com>
2023-02-06 15:10:07 +0200

    ViewSet for contacts created

--- apps/api_contacts/views.py
+++ apps/api_contacts/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from apps.contacts.models import Contact
+from .serializers import ContactsSerializer
+
+
+class ContactsViewSet(ModelViewSet):
+    serializer_class = ContactsSerializer
+    queryset = Contact.objects.all()
