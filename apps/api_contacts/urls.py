eb227dc733c37e217b5ae54b6cdda839492eea37
Axeratos <112187765+Axeratos@users.noreply.github.com>
2023-02-07 18:36:06 +0200

    Unused parts cutted

--- apps/api_contacts/urls.py
+++ apps/api_contacts/urls.py
@@ -6,8 +6,6 @@ from apps.api_contacts import views
 contacts_router = routers.DefaultRouter()
 contacts_router.register(r"contacts", views.ContactsViewSet, basename="contact")
 
-app_name = ""
-
 urlpatterns = [
     path("", include(contacts_router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
