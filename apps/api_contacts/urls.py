39b544054d8199e767f92cc688c260443c71bc3c
Axeratos <112187765+Axeratos@users.noreply.github.com>
2023-02-07 18:32:39 +0200

    Update urls.py

--- apps/api_contacts/urls.py
+++ apps/api_contacts/urls.py
@@ -8,8 +8,6 @@ contacts_router.register(r"contacts", views.ContactsViewSet, basename="contact")
 
 app_name = ""
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("", include(contacts_router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
